91c74023bf5e197693ab1987b024b2f41a80b65f
Konstantin Novikov <kvn746@gmail.com>
2021-04-18 22:12:22 +0300

    resource for articles added

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'index')->name('main');
 Route::view('/about', 'about')->name('about');
 
-Route::get('articles', 'ArticlesController@index')->name('articles.index');
-Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
-Route::get('articles/{article}', 'ArticlesController@show')->name('articles.show');
-Route::post('articles', 'ArticlesController@store')->name('articles.store');
-Route::get('articles/{article}/edit', 'ArticlesController@edit')->name('articles.edit');
-Route::patch('articles/{article}', 'ArticlesController@update')->name('articles.update');
-Route::delete('articles/{article}', 'ArticlesController@destroy')->name('articles.destroy');
+Route::resource('articles', 'ArticlesController');
 
 Route::get('contacts', 'ContactsController@index')->name('contacts.index');
 Route::get('contacts/create', 'ContactsController@create')->name('contacts.create');
